4720f940f49a9966655cefc109eb3165a93b010f
Mateusz Ledwoń <mateuszledwon@duck.com>
2026-03-28 17:28:43 +0100

    2026-03-28 17:28

--- home/.config/nvim/lua/plugins/dap.lua
+++ home/.config/nvim/lua/plugins/dap.lua
@@ -2,9 +2,10 @@ return {
   "mfussenegger/nvim-dap",
   dependencies = {
     {
-      "rcarriga/nvim-dap-ui",
-      main = "dapui",
-      opts = {},
+        "igorlfs/nvim-dap-view",
+        lazy = false,
+        version = "1.*",
+        opts = {},
     },
     {
       "leoluz/nvim-dap-go",
@@ -15,28 +16,10 @@ return {
   },
   keys = {
     {
-      "<leader>dh",
-      function() require("dapui").eval() end,
-      silent = true,
-      desc = "DAP eval hover",
-    },
-    {
-      "<leader>de",
-      function()
-        vim.ui.input({ prompt = "Expression: " }, function(input)
-          if input then
-            require("dapui").eval(input)
-          end
-        end)
-      end,
-      silent = true,
-      desc = "DAP eval expression",
-    },
-    {
       "<leader>dt",
-      function() require("dapui").toggle() end,
+      "<CMD>DapViewToggle<CR>",
       silent = true,
-      desc = "DAP UI toggle",
+      desc = "DAP view toggle",
     },
     {
       "<leader>dc",
@@ -62,35 +45,5 @@ return {
       silent = true,
       desc = "DAP toggle breakpoint",
     },
-    {
-      "<F1>",
-      function() require("dapui").toggle() end,
-      silent = true,
-      desc = "DAP UI toggle",
-    },
-    {
-      "<F2>",
-      function() require("dap").continue() end,
-      silent = true,
-      desc = "DAP continue",
-    },
-    {
-      "<F3>",
-      function() require("dap").toggle_breakpoint() end,
-      silent = true,
-      desc = "DAP toggle breakpoint",
-    },
   },
-  config = function()
-    local dap, dapui = require("dap"), require("dapui")
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      dapui.close()
-    end
-  end,
 }
